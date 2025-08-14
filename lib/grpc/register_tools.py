36cc508176674adf1e2e9f2bad59da253723312a
darren277 <darren277@yahoo.com>
2025-08-14 14:51:15 -0700

    Fixed name mismatch.

--- lib/grpc/register_tools.py
+++ lib/grpc/register_tools.py
@@ -2,7 +2,7 @@
 gRPC tool registrations for FHIR resources.
 """
 from google.fhir.proto.r5.core.resources import patient_pb2
-from lib.grpc.main import PatientController
+from lib.grpc.resources import PatientController
 from settings import FHIR_GRPC_HOST, FHIR_GRPC_PORT
 
 controller = PatientController(address=f"{FHIR_GRPC_HOST}:{FHIR_GRPC_PORT}", secure=True)
